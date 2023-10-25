1dd7cda9c849aefaab7988aa391530b5aaea3fdb
auxtin <austin.leonardo32@gmail.com>
2023-10-25 15:10:39 -0700

    replaced hashmap with two vectors, debugging 0,0 case

--- usaco/section_1.2/gift1/gift1.cpp
+++ usaco/section_1.2/gift1/gift1.cpp
@@ -10,17 +10,20 @@ LANG: C++
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
+#include <climits>
 
 using namespace std;
 
 int main()
 {
-    // ofstream fout("gift1.out");
+    ofstream fout("gift1.out");
     ifstream fin("gift1.in");
     string line, gifter = "";
     int count = 0, names, runningCount = INT_MIN;
-    unordered_map<string, int> members;
-    vector<string> args;
+    // unordered_map<string, int> members;
+    vector<int> balance;
+    vector<string> args, members;
     while (getline(fin, line))
     {
         // init # members
@@ -31,7 +34,8 @@ int main()
         // init 0 balance
         else if (count > 0 && count < names + 1)
         {
-            members[line] = 0;
+            members.push_back(line);
+            balance.resize(names, 0);
         }
         // person paying
         else
@@ -48,26 +52,36 @@ int main()
                     {
                         args.push_back(arg);
                     }
-                    if (stoi(args[0]) > 0)
+                    if (!args.empty() && stoi(args[0]) > 0)
                     {
-                        members[gifter] -= stoi(args[0]);
+                        auto it = find(members.begin(), members.end(), gifter);
+                        int idx = distance(members.begin(), it);
+                        balance[idx] -= stoi(args[0]);
+
                         runningCount = count + stoi(args[1]);
                         give = stoi(args[0]) / stoi(args[1]),
                         keep = stoi(args[0]) % stoi(args[1]);
-                        members[gifter] += keep;
-                        // cout << gifter << " is gifting " << give << " to " << args[1] << " people and keepin " << keep << endl;
+                        balance[idx] += keep;
                     }
+                    else
+                    {
+                        runningCount = count + 1;
+                        give = 0, keep = 0;
+                    }
+                    cout << gifter << " is gifting " << give << " to " << args[1] << " people and keeping " << keep << endl;
                 }
                 else
                 {
                     // process next lines to give the balance to resp members
                     if (count < runningCount + 1)
                     {
-                        members[line] += give;
-                        // cout << line << " will receive " << give << endl;
+                        auto it = find(members.begin(), members.end(), line);
+                        int idx = distance(members.begin(), it);
+                        balance[idx] += give;
+                        cout << line << " will receive " << give << endl;
                         if (count == runningCount)
                         {
-                            // cout << "===\n";
+                            cout << "===\n";
                             args.clear();
                             gifter = "";
                             runningCount = INT_MIN;
@@ -82,10 +96,9 @@ int main()
         }
         count++;
     }
-
-    for (auto &p : members)
+    for (int i = 0; i < names; i++)
     {
-        cout << p.first << "," << p.second << endl;
+        fout << members[i] << " " << balance[i] << "\n";
     }
     return 0;
 }
