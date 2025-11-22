a871d6d37b095eaf1ba08f7efe858bec2b5f63e6
auxtin <austin.leonardo32@gmail.com>
2025-11-21 19:59:46 -0800

    lucky number

---
+++ codeforces/110A - Lucky number/a.py
@@ -0,0 +1,13 @@
+def main():
+    n = int(input().strip())
+    count = 0
+    while n > 0:
+        if(n % 10 == 4 or n % 10 == 7):
+            count+=1
+        n //=10
+    if count == 4 or count == 7:
+        print("YES")
+    else:
+        print("NO")
+if __name__ == "__main__":
+    main()
